cd8efa2701d8436dfa0064490e740797ebb0f298
mbynmr <mbynmr@nottingham.ac.uk>
2023-05-11 11:37:43 +0100

    pre-verlet commit

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ def main():
     t_end = 2e-3
     dt = 1e-7
 
-    upscale_factor = 1e3  # python doesn't like small numbers so increase the size of everything!
+    upscale_factor = 1e3  # python doesn't enjoy such small numbers so increase the size of everything!
 
     element_width = 6e-5 * upscale_factor
     total_width = 6e-3 * upscale_factor
     # create a lock button where either one is locked. if you change one smoothly the other compensates (jumping near
     # to its actual value to get integer values)
 
-    b = 0  # damping coefficient
+    b = 0.99  # damping coefficient
+    b = 0
     c = 1e3 * upscale_factor  # speed
-    freq = 1.5e3  # driving frequency
+    # (0, 1) is at 1.31e4 for b=0
+    freq = 1.305e4 * 2.3  # driving frequency
     amp = 1e-5 * upscale_factor  # amplitude
     k = 1e9 * upscale_factor
     m = total_width ** 2 * (100e-6 * upscale_factor) * 1 * upscale_factor  # roughly correct expected mass of PDMS film
@@ -29,7 +31,7 @@ def main():
 
     # sim(dt, t_end, element_width, total_width, freq, amp, b, c)
     sim_dem(dt, t_end, element_width, total_width, freq, amp, b, k=k, dm=dm, g=g)
-    # basic()
+    basic()
     slice_viewer(t_end, element_width, total_width)
 
 
--- sim.py
+++ sim.py
@@ -87,6 +87,7 @@ def sim(dt, t_end, element_width, total_width, freq=2e3, amp=0.1, b=0, c=1e3):
             M[..., 1] = M[..., 2]
             # np.roll(M, shift=1, axis=2)  # any way to get the roll to work? It would be faster
 
+
 def sim_dem(dt, t_end, element_width, total_width, freq=2e3, amp=1e-5, b=0, k=1e6, dm=1e-9, g=0):
     # todo sort units everywhere, make sure it's all just in metres and seconds
     grid = [int(total_width / element_width), int(total_width / element_width)]
@@ -97,19 +98,11 @@ def sim_dem(dt, t_end, element_width, total_width, freq=2e3, amp=1e-5, b=0, k=1e
     # dt = 0.01 / (amp * freq ** 2)
     times = np.arange(int(t_end / dt)) * dt
 
-    # units
-    if b == 0:
-        damping = False
-    else:
-        damping = True
-
     # constants
     dx = element_width
     dx2 = dx ** 2
     km = - k / dm
     dt2 = dt ** 2
-
-    kernel = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
 
     # get indexes of membrane and support
     xm, ym = np.nonzero(shape)
@@ -119,7 +112,7 @@ def sim_dem(dt, t_end, element_width, total_width, freq=2e3, amp=1e-5, b=0, k=1e
 
     # predefine support positions to save calculation time, though this will be slower if the simulation is very long
     support = amp * np.sin(2 * np.pi * freq * times)
-    # M[..., 0] = (np.random.random(M[..., 0].shape) - 0.5) * amp / 1000  # todo remove this?
+    M[..., 0] = (np.random.random(M[..., 0].shape) - 0.5) * amp / 10  # todo remove this?
     M[xs, ys, 0] = amp * np.sin(2 * np.pi * freq * (0 - 2 * dt))
     M[xs, ys, 1] = amp * np.sin(2 * np.pi * freq * (0 - 1 * dt))
 
@@ -127,51 +120,30 @@ def sim_dem(dt, t_end, element_width, total_width, freq=2e3, amp=1e-5, b=0, k=1e
         for i, t in tqdm(enumerate(times), total=len(times)):
             M[xs, ys, 2] = support[i]
 
-            # todo try to use 2D grad to find differences!
-            #  also try to find angle using 2D grad if that is possible :o
-
-            # original widths ux
-            # tensioned widths dx
-            # displaced and tensioned: (grad)**2 + dx**2
-            #
-
-            if not damping:
-                # grads = np.gradient(M, axis=[0, 1])
-                # ggg = np.array(np.gradient(M[..., 1])).sum(axis=0)[xm, ym] / 2
-                # # ggg = np.array(np.gradient(M[..., 1])).mean(axis=0)[xm, ym]
-                # diff is smaller by 1, so pad by 1 on both sides and miss of the first element to get back to size.
-                diffsx = np.pad(np.diff(M[..., 1], axis=0), 1)[:, 1:-1]  # pad both then un-pad y
-                diffsy = np.pad(np.diff(M[..., 1], axis=1), 1)[1:-1, :]  # pad both then un-pad x
-                diffsx = diffsx[:-1, :] - diffsx[1:, :]
-                diffsy = diffsy[:, :-1] - diffsy[:, 1:]
-                diffs = diffsx + diffsy
-                M[xm, ym, 2] = diffs[xm, ym] * km * dt2 + (2 * M[xm, ym, 1] - M[xm, ym, 0]) - (g * dt2)
+            if b == 0:
+                dz_x = np.pad(np.diff(M[..., 1], axis=0), 1)[:, 1:-1]  # pad both then un-pad y
+                dz_y = np.pad(np.diff(M[..., 1], axis=1), 1)[1:-1, :]  # pad both then un-pad x
+                M[xm, ym, 2] = (dz_x[:-1, :] + dz_y[:, :-1] - (dz_x[1:, :] + dz_y[:, 1:])
+                                )[xm, ym] * km * dt2 + (2 * M[xm, ym, 1] - M[xm, ym, 0]) - (g * dt2)
 
                 # no convolution
-                # f1 = (np.sqrt((M[xm, ym, 1] - M[xm + 1, ym, 1]) ** 2 + dx2) - dx) * 2 * ((M[xm, ym, 1] - M[xm + 1, ym, 1] >= 0) - 0.5)
-                # f2 = (np.sqrt((M[xm, ym, 1] - M[xm, ym + 1, 1]) ** 2 + dx2) - dx) * 2 * ((M[xm, ym, 1] - M[xm, ym + 1, 1] >= 0) - 0.5)
-                # f3 = (np.sqrt((M[xm, ym, 1] - M[xm - 1, ym, 1]) ** 2 + dx2) - dx) * 2 * ((M[xm, ym, 1] - M[xm - 1, ym, 1] >= 0) - 0.5)
-                # f4 = (np.sqrt((M[xm, ym, 1] - M[xm, ym - 1, 1]) ** 2 + dx2) - dx) * 2 * ((M[xm, ym, 1] - M[xm, ym - 1, 1] >= 0) - 0.5)
+                # f1 = (np.sqrt((M[xm, ym, 1] - M[xm + 1, ym, 1]) ** 2 + dx2) - dx) * 2 * (
+                #         (M[xm, ym, 1] - M[xm + 1, ym, 1] >= 0) - 0.5)
                 # M[xm, ym, 2] = (f1 + f2 + f3 + f4) * km * dt2 + (2 * M[xm, ym, 1] - M[xm, ym, 0]) - (g * dt2)
                 # position and speed carried from previous frame: (2 * M[xm, ym, 1] - M[xm, ym, 0])
-
-                # pre-tensioned
-
-
-                # convolution: WRONG, pythag isn't going to work like this
-                # f = convolve2d(f, kernel, mode='same')
-                # f = np.sqrt(convolve2d(M[..., 1], kernel, mode='same') ** 2 + dx2) - dx
-                # M[xm, ym, 2] = f.reshape(M.shape[0:2])[xm, ym] * km * dt2 + 2 * M[xm, ym, 1] - M[xm, ym, 0]
-
             else:
-                print("not yet got damping")
+                dz_x = np.pad(np.diff(M[..., 1], axis=0), 1)[:, 1:-1]
+                dz_y = np.pad(np.diff(M[..., 1], axis=1), 1)[1:-1, :]
+                v = M[xm, ym, 1] - M[xm, ym, 0]
+                M[xm, ym, 2] = ((dz_x[:-1, :] + dz_y[:, :-1] - (dz_x[1:, :] + dz_y[:, 1:])
+                                 )[xm, ym] * km - b * v) * dt2 + M[xm, ym, 1] + v - (g * dt2)
             # recording data
             if i % 10 == 0:  # only write every ___ timestep
                 a = M[xm, ym, 2]
                 a = np.where(np.inf == np.where(a == np.NaN, 0, a), np.nanmax(a), a)
                 outputfile.writelines(f"{i} {t:.6g} {np.sqrt(np.mean(np.square(a))):.6g} {support[i]:.6g}\n")
                 # write step, time, rms pos of membrane, pos of support
-            if i % int((t_end / dt) / 250) == 0:  # write 500 total frames
+            if i % int((t_end / dt) / 1000) == 0:  # write 500 total frames
                 for n in range(M.shape[1]):
                     a = M[int(grid[0] / 2), n, 2]
                     a = np.where(np.inf == np.where(a == np.NaN, 0, a), np.nanmax(a), a)
